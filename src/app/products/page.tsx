0614324117f85109faea3285090620504ff2e240
RahulRex0 <75500427+RahulRex0@users.noreply.github.com>
2026-05-16 14:19:57 +0530

    added products details and designed product page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,45 @@
+import styles from "./page.module.css"
+import Image from "next/image"
+import products from "@/data/products.json"
+import Link from "next/link"
+
+export default function ProductPage(){
+    return(
+        <main>
+            <div className={styles.heading}>
+                Products
+            </div>
+            <div className={styles.catalog}>
+                {products.map((products)=>(
+                    <Link
+                         key={products.id}
+                         href={`/products/${products.id}`}
+                         className={styles.card}
+                    >
+                        <div>
+                            <div>
+                                <Image src={products.image} alt={products.name}
+                                className={styles.image}
+                                width={1000}
+                                height={400}
+                                />
+                            </div>
+                            <div className={styles.title}>
+                                {products.name}
+                            </div>
+                            <div>
+                                {products.description}
+                            </div>
+                        </div>
+
+                    </Link>
+                    
+
+                ))}
+             
+              
+
+            </div>
+        </main>
+    )
+}
