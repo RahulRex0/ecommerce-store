902fb57b75e7898f3c313e5e49258c57f63d229a
RahulRex0 <75500427+RahulRex0@users.noreply.github.com>
2026-05-23 13:31:11 +0530

    added css for product info page and added images of products in the product info page

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation"
 import styles from "./page.module.css"
 import AddToCartButton from "@/components/AddToCartButton";
 import Link from "next/link";
+import Image from "next/image";
 
 type Props = {
   params: Promise<{
@@ -18,27 +19,45 @@ export default async function ProductDetailPage({ params }: Props) {
       notFound();
     }
     return(
-      <div className={styles.container}>
-          <div className={styles.title}>
-            {product.name}
+      <main className={styles.page}>
+        <Link href="/products" className={styles.back} >
+          Back to Products
+        </Link>
+        <div className={styles.container}>
+          <div className={styles.imageFrame}>
+            <Image src={product.image} alt={product.name} width={500} height={500} className={styles.image}></Image>
           </div>
-          <div className={styles.description}>
-            {product.description}
-          </div>
-          <div className={styles.price}>
-            ₹{product.price}
-          </div>
-          <div>
-            Category: {product.category}
-          </div>
-          <div>
-            Stock:{product.stock}
-          </div>
-          <AddToCartButton product={product} />
-          <div>
-            <Link href="/cart"><button>Cart</button></Link>
-          </div>
-      </div>
+          <div className={styles.containerLeft}>
+            <div className={styles.title}>
+              <div className={styles.name}>
+              {product.name}
+              </div>
+              <div className={styles.description}>
+                {product.description}
+              </div>
+              <div className={styles.price}>
+                ₹{product.price}
+              </div>
+              <div>
+                Category: {product.category}
+              </div>
+              <div>
+                Stock:{product.stock}
+              </div>
+              <div className={styles.buttonContainer}>
+              <AddToCartButton product={product} className={styles.button}/>
+                <Link 
+                  href="/cart" 
+                  className={styles.button} 
+                  style={{ backgroundColor: "#fff", color: "black", border: "1px solid #ccc" }}
+                >
+                  Cart
+                </Link>
+              </div>
+              </div>
+            </div>
+        </div>
+      </main>
         
     )
   }
--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -10,13 +10,14 @@ type Product = {
 
 type Props = {
   product: Product;
+  className?: string;
 };
 
-export default function AddToCartButton({ product }: Props) {
+export default function AddToCartButton({ product, className }: Props) {
   const { addToCart } = useCart();
 
   return (
-    <button onClick={() => addToCart(product)}>
+    <button className={className} onClick={() => addToCart(product)}>
       Add to Cart
     </button>
   );
